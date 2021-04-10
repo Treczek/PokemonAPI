faf0184112a0b182c0912959b8112c4fce2704e5
Tomasz Reczek <reczet01@heiway.net>
2021-04-11 00:07:58 +0200

    Logger configuration

--- api/routes.py
+++ api/routes.py
@@ -4,8 +4,9 @@ Main module for Flask application.
 
 from flask import request
 from flask_restx import Resource, fields
+import logging
 
-from api import pokemon_api
+from api import pokemon_api, pokemon_encounters_api
 from api.utils.exceptions import NonExistingPokemon
 from api.services import PokemonService
 
@@ -38,3 +39,21 @@ class PokemonRoutes(Resource):
         except NonExistingPokemon:
             pokemon = PokemonService.fetch_from_pokeapi(json_data['name'])
             PokemonService.save_pokemon(pokemon)
+
+
+@pokemon_encounters_api.route('/<id>/encounters')
+class PokemonEncountersRoutes(Resource):
+
+    def get(self, id):
+        # Return all encounters
+        return id
+
+    def post(self, id):
+
+        logging.getLogger("PokemonAPI").info('Pokemon encounter has been posted')
+        # Inform the server about the encounter??
+        # Request should have optional note and obligatory place.
+        # Timestamp is added during object creation
+
+        print(id)
+        return id
